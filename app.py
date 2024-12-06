7957b2a82f64bd184b271bb4e4b490b70ce09120
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2024-12-06 13:58:22 +0100

    add exemple

--- app.py
+++ app.py
@@ -7,26 +7,41 @@ pickup_datetime = st.text_input(
     "Date et heure de prise en charge (format : YYYY-MM-DD HH:MM) *",
     key="pickup_datetime"
 )
+st.markdown("_Exemple : 2014-07-06 19:18:00_")
+
+
 pickup_longitude = st.number_input(
     "Longitude de prise en charge *",
     format="%.6f",
     key="pickup_longitude"
 )
+st.markdown("_Exemple : -73.950655_")
+
+
 pickup_latitude = st.number_input(
     "Latitude de prise en charge *",
     format="%.6f",
     key="pickup_latitude"
 )
+st.markdown("_Exemple : 40.783282_")
+
+
 dropoff_longitude = st.number_input(
     "Longitude de dépose *",
     format="%.6f",
     key="dropoff_longitude"
 )
+st.markdown("_Exemple : -73.984365_")
+
+
 dropoff_latitude = st.number_input(
     "Latitude de dépose *",
     format="%.6f",
     key="dropoff_latitude"
 )
+st.markdown("_Exemple : 40.769802_")
+
+
 passenger_count = st.number_input(
     "Nombre de passagers *",
     min_value=1,
@@ -34,6 +49,8 @@ passenger_count = st.number_input(
     step=1,
     key="passenger_count"
 )
+st.markdown("_Exemple : 2_")
+
 
 if st.button("Estimer votre trajet"):
     params = {
@@ -55,6 +72,8 @@ if st.button("Estimer votre trajet"):
                 fare = response.json().get("fare")
                 if fare is not None:
                     st.write(f"Votre trajet en taxi devrait coûter : {fare:.2f} $")
+                    st.markdown("_Si vous avez utilisé les données d'exemples, le résultat devrait être 14.71 ;)._")
+
                 else:
                     st.error("Veuillez réesayer")
             else:
